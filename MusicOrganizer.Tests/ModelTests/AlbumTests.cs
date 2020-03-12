7a22f2f3f01f0ccceae36899dd68c0e9c8b5590d
Drake Wilcox and Dusty McCord <drake+dusty@>
2020-03-12 10:36:48 -0700

    add passing tests for Album Model, Getall, Find, Update,Delete.

---
+++ MusicOrganizer.Tests/ModelTests/AlbumTests.cs
@@ -0,0 +1,79 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using MusicOrganizer.Models;
+using System;
+
+
+namespace MusicOrganizer.Tests
+{
+
+  [TestClass]
+  public class AlbumTest : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Album.ResetAll();
+    }
+
+    [TestMethod]
+    public void AlbumConstructor_CreateInstanceOfAlbum_Album()
+    {
+      Album newAlbum = new Album("TestTitle", 1977, "Subpop", "album.jpg");
+      Assert.AreEqual(typeof(Album), newAlbum.GetType());
+    }
+
+    [TestMethod]
+    public void AlbumConstructor_AutoAddsToAlbumList_List()
+    {
+      Album newAlbum = new Album("TestTitle", 1977, "Subpop", "album.jpg");
+      List<Album> testList = new List<Album>() { newAlbum };
+      List<Album> returnedList = Album.GetAll();
+
+      CollectionAssert.AreEqual(testList, returnedList);
+    }
+
+    [TestMethod]
+    public void Find_ReturnSingleAlbumFromList_Album()
+    {
+      Album album0 = new Album("Tom and the boys", 1977, "Subpop", "album.jpg");
+      Album album1 = new Album("Just Jerry", 1977, "Subpop", "album.jpg");
+
+      List<Album> returnedList = Album.GetAll();
+
+      Album result = Album.Find(1);
+
+      Assert.AreEqual(album1, result);
+    }
+
+    [TestMethod]
+    public void Update_WillUpdateProperties_void()
+    {
+      Album album0 = new Album("Tom and the boys", 1977, "Subpop", "album.jpg");
+      string title = "Jerry and the boys";
+
+      Album.Update(0, title, 1977, "Subpop", "All");
+      Album testAlbum = Album.Find(0);
+
+      Assert.AreEqual(title, testAlbum.Title);
+    }
+
+    [TestMethod]
+    public void Delete_WillDeleteItemFromList_void()
+    {
+      Album album0 = new Album("Tom and the boys", 1977, "Subpop", "album.jpg");
+      Album album1 = new Album("Just Jerry", 1977, "Subpop", "album.jpg");
+      Album album3 = new Album("Jerry and the Fam", 1977, "Subpop", "album.jpg");
+
+      Album.Delete(1);
+
+      List<Album> testList = new List<Album> { album0, album3 };
+      List<Album> result = Album.GetAll();
+
+
+
+      CollectionAssert.AreEqual(testList, result);
+    }
+  }
+
+}
--- MusicOrganizer/Models/Album.cs
+++ MusicOrganizer/Models/Album.cs
@@ -1,12 +1,64 @@
 using System.Collections.Generic;
+using System;
 
 namespace MusicOrganizer.Models
 {
   public class Album
   {
-    public string Name { get; set; }
+    public string Title { get; set; }
     public int Year { get; set; }
     public string Label { get; set; }
     public string Img { get; set; }
+    public int Id { get; }
+    private static List<Album> _instances = new List<Album> { };
+    private static int _count = 0;
+
+    public Album(string title, int year, string label, string img)
+    {
+      Title = title;
+      Year = year;
+      Label = label;
+      Img = img;
+      _instances.Add(this);
+      Id = _count++;
+
+    }
+    public static List<Album> GetAll()
+    {
+
+      return _instances;
+    }
+    public static void ResetAll()
+    {
+      _count = 0;
+      _instances.Clear();
+    }
+
+    public static Album Find(int id)
+    {
+      foreach (Album album in _instances)
+      {
+        if (album.Id == id)
+        {
+          return album;
+        }
+      }
+      return _instances[0];
+    }
+
+    public static void Update(int id, string title, int year, string label, string img)
+    {
+      Album result = Find(id);
+      result.Title = title;
+      result.Year = year;
+      result.Label = label;
+      result.Img = img;
+    }
+
+    public static void Delete(int id)
+    {
+      Album toRemove = Find(id);
+      _instances.Remove(toRemove);
+    }
   }
 }
