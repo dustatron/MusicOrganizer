76026f6e037f09696e4e1ea745f5ec57b7509b4a
Drake Wilcox and Dusty McCord <drake+dusty@>
2020-03-12 09:44:04 -0700

    add fields and contructor.

---
+++ MusicOrganizer/Models/Album.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Models
+{
+  public class Album
+  {
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public string Label { get; set; }
+    public string Img { get; set; }
+  }
+}
